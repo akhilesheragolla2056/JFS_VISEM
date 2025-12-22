23e083db5fc3f7dbf092fdcc14cccf0cde23954a
Eragolla Akhilesh <akhilesheragolla2056@gmail.com>
2025-12-22 10:31:15 +0530

    Upgrade to Java 21 and migrate from javax to jakarta servlet API

--- Maven_Projects/EmployeeWebApp/src/main/java/com/skillnext1/employee/EmployeeServlet.java
+++ Maven_Projects/EmployeeWebApp/src/main/java/com/skillnext1/employee/EmployeeServlet.java
@@ -1,7 +1,7 @@
 package com.skillnext1.employee;
 
-import javax.servlet.*;
-import javax.servlet.http.*;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
 import java.io.IOException;
 
 public class EmployeeServlet extends HttpServlet {
